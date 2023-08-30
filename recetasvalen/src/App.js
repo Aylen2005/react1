943fb0e89d1b499c3f75a152a18afc9ad6fcc742
Valentina <vabusto@escuelasproa.edu.ar>
2023-08-30 08:54:43 -0300

    se agrega tarjetas

--- recetasvalen/src/App.js
+++ recetasvalen/src/App.js
@@ -1,5 +1,11 @@
 import logo from './logo.png';
 import './App.css';
+import './tarjetas.js';
+import flan from './flan.jpg';
+import { Card } from 'react-bootstrap';
+
+
+
 
 function App() {
   return (
@@ -14,9 +20,19 @@ function App() {
       </div>
       
       <header className="App-header">
-        <card>
-          <h1> holi como tan</h1>
-        </card>
+        <Card
+          imagenSrc={flan} alt='flan'
+          title="Chocotorta en simcdples pasos"
+          description="Mezcla el dulce de leche con el queso crema.Luego lo unes todo hasta que tengas una crema lisa y sin grumos.
+           A continuación, en un plato, pon el café y el agua. El café puede ser con cafeína o descafeinado, como prefieras. En caso de no gustarte, puedes poner leche para mojar las galletas.
+          La primera capa, la pondremos de galletas, que será la base de la tarta.Agarra las galletas, las vas pasando brevemente por la mezcla de café y agua y las colocas ordenadas en el fondo del molde.
+          Procura rellenar bien todos los huecos con trocitos de galletas cortadas.A continuación pon una capa de la mezcla de dulce de leche y queso crema.
+          Tienes que extender bien la capa, procurando que cubra todas las galletas.A continuación pon otra capa de galletas. Igual que antes, mojas y colocas.Y otra capa de crema.
+          Así hasta que llegues a la última capa que será de dulce de leche.
+          Una vez terminado, se lleva al refrigerador. Y listo!"
+          
+        />
+        
       </header>
       <footer className='footer'>
         <p className="footer-copyright">© 2023 recetas. Todos los derechos reservados.</p>
@@ -24,5 +40,5 @@ function App() {
     </div>
   );
 }
-
 export default App;
+
---
+++ recetasvalen/src/tarjetas.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+const Card = (props) => {
+  const { imagenSrc, title, description, iconPath1,iconPath2,iconPath3,iconPath4, iconText1,iconText2,iconText3,iconText4} = props;
+
+  return (
+    <div className="recipe-card">
+    <nav>
+      <ul className="card-content">
+        <li><a href="#"></a></li>
+        <div className="card-img">
+          <img src={imagenSrc} alt="Receta" />
+        </div>
+        <div className="card-info">
+          <h5 className="card-title">{title}</h5>
+          <p className="card-text">{description}</p>
+        </div>
+        <div className="more-info-icons">
+        <div className="icon">
+          <img src={iconPath1}  />
+          <span>{props.iconText1}</span>
+        </div>
+        <div className="icon">
+          <img src={iconPath2}  />
+          <span>{props.iconText2}</span>
+        </div>
+        <div className="icon">
+          <img src={iconPath3}  />
+          <span>{props.iconText3}</span>
+        </div>
+        <div className="icon">
+          <img src={iconPath4} />
+          <span>{props.iconText4}</span>
+        </div>
+</div>
+      </ul>
+    </nav>
+  </div>
+  
+  );
+  }  
+export default Card;
+
+
